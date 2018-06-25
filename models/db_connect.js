c5544eef40ba975b835c74a766eeeb425cc12003
Umang <umangpahwa@gmail.com>
2018-06-25 20:03:12 +0530

    db to mlab

--- models/db_connect.js
+++ models/db_connect.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 //Set up default mongoose connection
-const mongoDB = 'mongodb://127.0.0.1/hack_db';
+const mongoDB = 'mongodb://umaniax:umianu1234@ds263640.mlab.com:63640/cloneheonnode';
 mongoose.connect(mongoDB);
 mongoose.Promise = Promise;
 
